c90ee3361a9aeaceb8fc4461fe5fa3a31092c833
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2025-12-01 19:07:53 +0530

    Add files via upload

---
+++ 9_Foreign_key.sql
@@ -0,0 +1,47 @@
+use startersql;
+drop table if exists addresses;
+create table addresses(
+	id int auto_increment primary key,
+    user_id int,
+    street varchar(250),
+    city varchar(100),
+    state varchar(100),
+    pincode varchar(10),
+    constraint fk_user foreign key (user_id) references user4(id) on delete cascade
+    -- on cascade means: if any id row is deleted from parent table then the address will also deleted
+    -- you can also user "set null cascade" if row deleted from parent then now it will set as null
+    );
+
+select * from user4;
+insert into addresses(user_id, street, city, state, pincode)
+values
+(1, '221B MG Road', 'Mumbai', 'Maharashtra', '400001'),
+(2, '14 Park Street', 'Kolkata', 'West Bengal', '700016'),
+(3, '32 Residency Road', 'Bengaluru', 'Karnataka', '560025'),
+(4, '5 North Usman Road', 'Chennai', 'Tamil Nadu', '600017'),
+(5, '17 Hazratganj', 'Lucknow', 'Uttar Pradesh', '226001'),
+(6, '55 Banjara Hills', 'Hyderabad', 'Telangana', '500034'),
+(7, '88 Connaught Place', 'Delhi', 'Delhi', '110001'),
+(8, '10 MG Marg', 'Dehradun', 'Uttarakhand', '248001'),
+(9, '23 Brigade Road', 'Bengaluru', 'Karnataka', '560025'),
+(10, '45 Marine Drive', 'Mumbai', 'Maharashtra', '400020'),
+(11, '67 Ashoka Road', 'Delhi', 'Delhi', '110001'),
+(12, '89 MG Road', 'Pune', 'Maharashtra', '411001'),
+(13, '12 Brigade Road', 'Bengaluru', 'Karnataka', '560025'),
+(14, '34 Park Street', 'Kolkata', 'West Bengal', '700016'),
+(15, '56 Connaught Place', 'Delhi', 'Delhi', '110001'),
+(16, '78 Marine Drive', 'Mumbai', 'Maharashtra', '400020'),
+(17, '90 MG Marg', 'Dehradun', 'Uttarakhand', '248001'),
+(18, '11 North Usman Road', 'Chennai', 'Tamil Nadu', '600017'),
+(19, '33 Residency Road', 'Bengaluru', 'Karnataka', '560025'),
+(20, '22 Hazratganj', 'Lucknow', 'Uttar Pradesh', '226001');
+
+select * from user4;
+select * from addresses;
+
+delete from addresses where id=1;
+-- if we delete the addreess then user will not delete
+
+delete from user4 where id=3;
+-- if we delete user then the address will also delete
+
