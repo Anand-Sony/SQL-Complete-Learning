1a1823d7a54645ea5a43a117dea96f38a4401d8e
Anand Soni <137056068+Anand-Sony@users.noreply.github.com>
2025-12-01 19:06:42 +0530

    Add files via upload

---
+++ 4_updating_data.sql
@@ -0,0 +1,56 @@
+create database if not exists startersql;
+use startersql;
+create table user2(
+	id int auto_increment primary key,
+    name varchar(100) not null,
+    email varchar(100) not null,
+    gender enum("Male", "Female", "Other"),
+    date_of_birth date,
+    salary decimal(10, 2),
+    created_at timestamp default current_timestamp
+);
+INSERT INTO user2 (name, email, gender, date_of_birth, salary) VALUES
+('Aarav', 'aarav@example.com', 'Male', '1995-05-14', 65000.00),
+('Ananya', 'ananya@example.com', 'Female', '1990-11-23', 72000.00),
+('Raj', 'raj@example.com', 'Male', '1988-02-17', 58000.00),
+('Sneha', 'sneha@example.com', 'Female', '2000-08-09', 50000.00),
+('Farhan', 'farhan@example.com', 'Male', '1993-12-30', 61000.00),
+('Priyanka', 'priyanka@example.com', 'Female', '1985-07-12', 84000.00),
+('Aisha', 'aisha@example.com', 'Female', '1997-03-25', 56000.00),
+('Aditya', 'aditya@example.com', 'Male', '1992-06-17', 69000.00),
+('Meera', 'meera@example.com', 'Female', '1989-09-05', 77000.00),
+('Ishaan', 'ishaan@example.com', 'Male', '2001-10-02', 45000.00),
+('Tanvi', 'tanvi@example.com', 'Female', '1994-04-18', 62000.00),
+('Rohan', 'rohan@example.com', 'Male', '1986-12-01', 75000.00),
+('Zoya', 'zoya@example.com', 'Female', '1998-01-15', 54000.00),
+('Karan', 'karan@example.com', 'Male', '1990-08-22', 68000.00),
+('Nikita', 'nikita@example.com', 'Female', '1987-03-10', 71000.00),
+('Manav', 'manav@example.com', 'Male', '1996-11-29', 61000.00),
+('Divya', 'divya@example.com', 'Female', '1991-02-28', 57000.00),
+('Harshit', 'harshit@example.com', 'Male', '1993-09-09', 65000.00),
+('Ritika', 'ritika@example.com', 'Female', '1999-05-05', 52000.00),
+('Imran', 'imran@example.com', 'Male', '1995-07-30', 63000.00),
+('Juhi', 'juhi@example.com', 'Female', '1992-10-14', 59000.00),
+('Tushar', 'tushar@example.com', 'Male', '1990-01-08', 73000.00),
+('Lata', 'lata@example.com', 'Female', '1984-11-11', 78000.00),
+('Yash', 'yash@example.com', 'Male', '1997-06-06', 64000.00),
+('Fatima', 'fatima@example.com', 'Female', '1993-03-03', 55000.00);
+
+select * from user2;
+
+-- now updating the data
+
+update user2 set salary =45000 where id=1;
+select * from user2;
+
+update user2 set gender= "Female" where id=1;
+select * from user2;
+
+update user2 set date_of_birth="2000-05-14", name="Arav" where id=1;
+select * from user2;
+
+-- increase salary by 10k whose salary is less than 60k
+update user2 set salary=salary+10000 where salary<=60000 and id>0;
+select * from user2;
+-- now lets check ishaan salaray earlier it was 45000
+select * from user2 where name="Ishaan";
